b30a16a1b2b19806cee11a491654b4a89e795cc1
ryanabrowning <ryanbrowning@Ryans-MacBook-Pro.local>
2018-03-13 11:10:40 -0500

    fix list item

--- components/discovery/list/ListItem.js
+++ components/discovery/list/ListItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Image, StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { ListItem as NativeListItem } from 'react-native-elements';
 
 
 const ListItem = ({ onPress, item }) => {
 	const imageURL = item.images && item.images.length > 0 ? item.images[0].secure_url : null;
 	return (
-		<ListItem
+		<NativeListItem
 			avatar={imageURL && <Image style={{ width: 50, height: 50 }} source={{ uri: imageURL }} />}
 			title={item.title}
 			titleStyle={styles.title}
@@ -23,11 +23,11 @@ const styles = StyleSheet.create({
 	}
 });
 
-EventItem.propTypes = {
+ListItem.propTypes = {
 	onPress: PropTypes.func.isRequired,
 	item: PropTypes.shape({
 		title: PropTypes.string
 	}).isRequired
 };
 
-export default EventItem;
+export default ListItem;
