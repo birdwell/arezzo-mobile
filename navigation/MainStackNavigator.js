acfa65a2bda54efe89363729410aebc5ec37e793
Nathan Popp <Nathan.E.Popp-1@ou.edu>
2018-04-20 18:22:06 -0500

    Adds Detailed views for Food, Outdoor, Shopping, and Sight

--- navigation/MainStackNavigator.js
+++ navigation/MainStackNavigator.js
@@ -2,6 +2,9 @@ import { StackNavigator } from 'react-navigation';
 import HomeScreen from '../screens/HomeScreen';
 import EventDetails from '../screens/details/EventDetails';
 import FoodDetails from '../screens/details/FoodDetails';
+import ShoppingDetails from '../screens/details/ShoppingDetails';
+import SightDetails from '../screens/details/SightDetails';
+import OutdoorDetails from '../screens/details/OutdoorDetails';
 import PlaceScreen from '../screens/PlaceScreen';
 import MoreScreen from '../screens/MoreScreen';
 
@@ -20,6 +23,15 @@ const MainStackNavigator = StackNavigator({
 	},
 	FoodDetails: {
 		screen: FoodDetails
+	},
+	ShoppingDetails: {
+		screen: ShoppingDetails
+	},
+	SightDetails: {
+		screen: ShoppingDetails
+	},
+	OutdoorDetails: {
+		screen: ShoppingDetails
 	}
 });
 
